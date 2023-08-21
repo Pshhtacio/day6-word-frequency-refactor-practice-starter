11a1ce0dee29b5764bbf1ca2c4fe448de550e73b
Mhark Joseph <mharkjgalang@gmail.com>
2023-08-21 17:16:36 +0800

    refactor: change Input class to WordFrequencyInfo

---
+++ src/main/java/WordFrequencyInfo.java
@@ -0,0 +1,16 @@
+public class WordFrequencyInfo {
+    private String word;
+    private int count;
+    public WordFrequencyInfo(String w, int i) {
+        this.word = w;
+        this.count = i;
+    }
+    public String getWord() {
+        return this.word;
+    }
+    public int getWordCount() {
+        return this.count;
+    }
+
+
+}
